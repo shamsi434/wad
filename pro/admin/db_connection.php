573e8ecdc6cd0500cc7e0fe22b029ca129121ec7
Shams uz Zaman <L1F16BSCS0084>
2019-01-08 19:15:23 +0500

    Quiz 5 wad class

--- pro/admin/db_connection.php
+++ pro/admin/db_connection.php
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 <?php
 $con = mysqli_connect("localhost","root","","tech_box_db");
 if(!$con)
-=======
-<?php
 $con = mysqli_connect("localhost","root","","phpFirstTask");
-if(!$con)
->>>>>>> 60ea7e07936548f155351723dece4bb80df6072d
+
     die("Connection failed");
